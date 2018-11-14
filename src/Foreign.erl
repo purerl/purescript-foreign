e333842696f9b20a2f00299a70b9cca7667943f9
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2018-11-14 13:39:27 +0000

    Fix FFI for erl typeOf

--- src/Foreign.erl
+++ src/Foreign.erl
@@ -5,17 +5,17 @@ unsafeToForeign(X) -> X.
 
 unsafeFromForeign(X) -> X.
 
-typeOf(X) when is_integer(X) -> "integer";
-typeOf(X) when is_float(X) -> "float";
-typeOf(X) when is_boolean(X) -> "boolean";
-typeOf(X) when is_atom(X) -> "atom";
-typeOf(X) when is_binary(X) -> "binary";
-typeOf(X) when is_bitstring(X) -> "bitstring";
-typeOf(X) when is_list(X) -> "list";
-typeOf(X) when is_map(X) -> "map";
-typeOf(X) when is_tuple(X) -> "tuple";
-typeOf(X) when is_pid(X) -> "pid";
-typeOf(X) -> "term".
+typeOf(X) when is_integer(X) -> <<"integer">>;
+typeOf(X) when is_float(X) -> <<"float">>;
+typeOf(X) when is_boolean(X) -> <<"boolean">>;
+typeOf(X) when is_atom(X) -> <<"atom">>;
+typeOf(X) when is_binary(X) -> <<"binary">>;
+typeOf(X) when is_bitstring(X) -> <<"bitstring">>;
+typeOf(X) when is_list(X) -> <<"list">>;
+typeOf(X) when is_map(X) -> <<"map">>;
+typeOf(X) when is_tuple(X) -> <<"tuple">>;
+typeOf(X) when is_pid(X) -> <<"pid">>;
+typeOf(X) -> <<"term">>.
 
 tagOf(X) -> typeOf(X).
 
